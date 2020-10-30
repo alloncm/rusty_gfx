daa59f88498c3086ac5fbfa5a0a14307b14da8d4
alloncm <alloncm@gmail.com>
2020-10-30 15:34:24 +0200

    Implement the audio queueing
    The user pushes new data to the struct instead of the struct
    reqiesting the data through callback.
    
    Implemented it through raw SDL so be carefull

--- src/audio.rs
+++ src/audio.rs
@@ -1,14 +1,25 @@
 use sdl2::sys;
-
+use std::ffi::c_void;
+use std::ffi::CStr;
+use sdl2::libc::c_char;
+use std::mem::MaybeUninit;
 
 pub struct Audio{
     device_id: sys::SDL_AudioDeviceID,
-    frequency:i32,
-    channels:u8
+    pub frequency:i32,
+    pub channels:u8
 }
 
 impl Audio{
-    pub fn new(freq:i32, channels:u8, fps:u8)->Self{
+    pub fn init(freq:i32, channels:u8, fps:u16)->Self{
+
+        unsafe{
+            sys::SDL_ClearError();
+            if sys::SDL_InitSubSystem(sys::SDL_INIT_AUDIO) != 0{
+                std::panic!("{}", Self::get_sdl_error_message());
+            }
+        }
+
         let samples = (freq / fps as i32) as u16;
         let desired_audio_spec = sys::SDL_AudioSpec{
             freq: freq ,
@@ -22,18 +33,56 @@ impl Audio{
             userdata:std::ptr::null_mut()
         };
     
-        let audio_spec:*mut sys::SDL_AudioSpec = std::ptr::null_mut();
+        let mut uninit_audio_spec:MaybeUninit<sys::SDL_AudioSpec> = MaybeUninit::uninit();
 
         unsafe{
-            let device_id = sys::SDL_OpenAudioDevice(std::ptr::null(), 0, &desired_audio_spec, audio_spec , 0);
+            sys::SDL_ClearError();
+            let device_id = sys::SDL_OpenAudioDevice(std::ptr::null(), 0, &desired_audio_spec, uninit_audio_spec.as_mut_ptr() , 0);
 
-            let audio = Audio{
-                frequency:(*audio_spec).freq,
-                channels: (*audio_spec).channels,
+            if device_id == 0{
+                std::panic!("{}",Self::get_sdl_error_message());
+            }
+
+            let init_audio_spec:sys::SDL_AudioSpec = uninit_audio_spec.assume_init();
+
+            sys::SDL_PauseAudioDevice(device_id, 0);
+
+            return Audio{
+                frequency:init_audio_spec.freq,
+                channels: init_audio_spec.channels,
                 device_id:device_id
             };
+        }
+    }
 
-            return audio;
+    pub fn push_audio_to_device(&self, audio:&[f32])->Result<(),&str>{
+        unsafe{
+            let audio_ptr: *const c_void = audio.as_ptr() as *const c_void;
+            let data_byte_len = (audio.len() * std::mem::size_of::<f32>()) as u32;
+
+            sys::SDL_ClearError();
+            if sys::SDL_QueueAudio(self.device_id, audio_ptr, data_byte_len) != 0{
+                return Err(Self::get_sdl_error_message());
+            }
+            
+            Ok(())
+        }
+    }
+
+    fn get_sdl_error_message()->&'static str{
+        unsafe{
+            let error_message:*const c_char = sys::SDL_GetError();
+            
+            return CStr::from_ptr(error_message).to_str().unwrap();
+        }
+    }
+}
+
+impl Drop for Audio{
+    fn drop(&mut self) {
+        unsafe{
+            sys::SDL_CloseAudioDevice(self.device_id);
+            sys::SDL_QuitSubSystem(sys::SDL_INIT_AUDIO);
         }
     }
 }
